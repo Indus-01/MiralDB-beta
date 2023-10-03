a3b1ab2bb776af8aa77b619fd9923ece6e1012ab
Miral Abbastada <121128789+Indus-01@users.noreply.github.com>
2023-10-03 19:42:56 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('.text-input-indus-default');
+const btn = document.querySelector('.indus-post-button');
+const textarea = document.querySelector('.text-area-indus-default');
+const cardBody = document.querySelector('.container-cards');
+
+const users = [
+       {
+              name1: 'El Abdullo Abi Shalmurad',
+              age: 23,
+       },
+       {
+              name1: 'El Alex',
+              age: 23,
+       }
+];
+
+const postTodo = () => {
+       document.querySelector('#modal').style.display = 'block';
+}
+
+document.getElementById('close').onclick = function(){
+       document.querySelector('#modal').style.display = 'none';
+}
+
+const promise = new Promise((resolve, reject) => {
+
+});
+
+console.log(promise);
